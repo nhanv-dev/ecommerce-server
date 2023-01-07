fac01a36cb40fd49ae58d4ffc2f7b10619cbe1f3
Trần Thanh Nhân <66198815+nhanv-dev@users.noreply.github.com>
2023-01-08 01:26:18 +0700

    update cors

--- index.js
+++ index.js
@@ -13,7 +13,13 @@ dotenv.config();
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cors());
-
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://ecommerce-d49f8.web.app/');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 // app.use(function (req, res, next) {
 //     res.io = socket;
